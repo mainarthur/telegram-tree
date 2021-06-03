849a59d15dd0d4ae4d2fa242006fef80c6addc51
Arthur Kh <listbox6@gmail.com>
2021-06-03 16:29:47 +0300

    add type validation for post model

--- db/models/Post.js
+++ db/models/Post.js
@@ -1,5 +1,18 @@
 class Post {
+  /**
+   * @param {Object} param
+   * @param {String} param.name
+   * @param {String} param.link
+   * @param {String} [param.description]
+   * @param {number} [param.id]
+   */
   constructor({ id, name, description, link }) {
+    if (typeof name !== "string") throw new Error('"name" must be a string');
+    if (typeof link !== "string") throw new Error('"link" must be a string');
+    if (typeof id !== "number" && id !== undefined)
+      throw new Error('"id" must be a number');
+    if (typeof description !== "string" && description !== undefined)
+      throw new Error('"description" must be a string');
     this.id = id;
     this.name = name;
     this.description = description;
